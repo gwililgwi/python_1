75a6abe26d0486d36d905a0a00057956427ca709
gwililgwi <80652061+gwililgwi@users.noreply.github.com>
2021-05-10 22:11:17 +0900

    Add files via upload

---
+++ first.py
@@ -0,0 +1,46 @@
+# print("Hello world")
+# print("50")
+# print([50]+[50])
+# print(50, 50, 50)
+
+# i = 5
+# while i>0:
+#     print(i)
+#     i=i-1
+
+# i = 0
+# while True:
+#     print("knock!")
+#     if i >= 3:
+#         break
+#     i = i+1 # if 로 break를 걸어주어 조건을 만족하면 멈추게 만든다
+
+
+# # 문제1
+# i=1
+# while i<11:
+#     print(i,"년째 수감 중입니다.")
+#     i= i+1
+# print("감옥에서 석방되었습니다!")
+
+# # 문제2
+# check = int(input())
+# i = 0
+# s = "*"
+# while i <= check:
+#     print(i*s)
+#     i=i+1
+
+# ans = 0
+# i = 0
+# while True:
+#     number = int(input())
+#     ans = ans + number
+#     i = i +1
+#     if number == 0:
+#         ans/=i-1
+#         print(ans)
+#         break
+
+
+    
